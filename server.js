291f4f3201470d104f0ad83b3eed27fef4dc8f7f
somsakKaeworasan <somsak.kaeworasan@outlook.com>
2022-02-23 08:06:34 +0700

    Add - app.route - vaccines postman.

--- server.js
+++ server.js
@@ -5,6 +5,80 @@ const config = require('./core/config');
 
 app.use(express.json());
 
+app.route('/vaccines')
+.get((req, res) => {
+
+    const id = req.query.id
+
+    const vaccine  = vaccineList.find(f => f.id == id);
+
+    if (!id){
+        res.status(200).json(vaccineList);
+    }else{
+        if (vaccine){
+            res.status(200).json(vaccine);
+        }else{
+            res.status(404).end();
+        }
+    }
+})
+.post((req, res) => {
+
+    const {code, name, ef} = req.body;
+    const newID = getNextID();
+
+    vaccineList.push({
+        id: newID,
+       code,
+       name,
+       ef
+    })
+
+    res.redirect(`/vaccines?id=${newID}`)
+
+    // res.json({
+    //    code,
+    //    name,
+    //    ef
+    // })
+})
+.put((req, res) => {
+
+    const id = req.query.id;
+    const {code, name, ef} = req.body;
+
+    const index = vaccineList.findIndex(f => f.id == id);
+
+    if (index > 0){
+        vaccineList[index].code = code
+        vaccineList[index].name = name
+        vaccineList[index].ef = ef
+
+        res.status(200).json({
+            vaccineList: vaccineList,
+            des: "Update data successfully."
+        });
+    }else{
+        res.status(404).json({
+            des: "No update data found."
+        });
+    }
+})
+.delete((req, res) => {
+
+    const id = req.query.id;
+    const index = vaccineList.findIndex(f => f.id == id);
+
+    if (index > 0){
+        vaccineList.splice(index, 1)
+        res.status(200).json(vaccineList);
+    }else{
+        res.status(404).json({
+            des: "No delete data found."
+        });
+    }
+});
+
 // app.get('/', function (req, res) {
 //   res.send('Hello World somsak')
 // });
@@ -39,23 +113,22 @@ app.use(express.json());
 //     })
 // });
 
-app.get('/vaccines', (req, res) => {
+// app.get('/vaccines', (req, res) => {
 
-    const id = req.query.id
+//     const id = req.query.id
 
-    const vaccine  = vaccineList.find(f => f.id == id);
+//     const vaccine  = vaccineList.find(f => f.id == id);
 
-    if (!id){
-        res.status(200).json(vaccineList);
-    }else{
-        if (vaccine){
-            res.status(200).json(vaccine);
-        }else{
-            res.status(404).end();
-        }
-    }
-    
-});
+//     if (!id){
+//         res.status(200).json(vaccineList);
+//     }else{
+//         if (vaccine){
+//             res.status(200).json(vaccine);
+//         }else{
+//             res.status(404).end();
+//         }
+//     }
+// });
 
 // app.post('/vaccines', (req, res) => {
 
@@ -74,26 +147,26 @@ app.get('/vaccines', (req, res) => {
 //     })
 // });
 
-app.post('/vaccines', (req, res) => {
+// app.post('/vaccines', (req, res) => {
 
-    const {code, name, ef} = req.body;
-    const newID = getNextID();
+//     const {code, name, ef} = req.body;
+//     const newID = getNextID();
 
-    vaccineList.push({
-        id: newID,
-       code,
-       name,
-       ef
-    })
+//     vaccineList.push({
+//         id: newID,
+//        code,
+//        name,
+//        ef
+//     })
 
-    res.redirect(`/vaccines?id=${newID}`)
+//     res.redirect(`/vaccines?id=${newID}`)
 
-    // res.json({
-    //    code,
-    //    name,
-    //    ef
-    // })
-});
+//     // res.json({
+//     //    code,
+//     //    name,
+//     //    ef
+//     // })
+// });
 
 // app.put('/vaccines', (req, res) => {
 
@@ -106,43 +179,43 @@ app.post('/vaccines', (req, res) => {
 //     })
 // });
 
-app.put('/vaccines', (req, res) => {
+// app.put('/vaccines', (req, res) => {
 
-    const id = req.query.id;
-    const {code, name, ef} = req.body;
+//     const id = req.query.id;
+//     const {code, name, ef} = req.body;
 
-    const index = vaccineList.findIndex(f => f.id == id);
+//     const index = vaccineList.findIndex(f => f.id == id);
 
-    if (index > 0){
-        vaccineList[index].code = code
-        vaccineList[index].name = name
-        vaccineList[index].ef = ef
+//     if (index > 0){
+//         vaccineList[index].code = code
+//         vaccineList[index].name = name
+//         vaccineList[index].ef = ef
 
-        res.status(200).json({
-            vaccineList: vaccineList,
-            des: "Update data successfully."
-        });
-    }else{
-        res.status(404).json({
-            des: "No update data found."
-        });
-    }
-});
+//         res.status(200).json({
+//             vaccineList: vaccineList,
+//             des: "Update data successfully."
+//         });
+//     }else{
+//         res.status(404).json({
+//             des: "No update data found."
+//         });
+//     }
+// });
 
-app.delete('/vaccines', (req, res) => {
+// app.delete('/vaccines', (req, res) => {
 
-    const id = req.query.id;
-    const index = vaccineList.findIndex(f => f.id == id);
+//     const id = req.query.id;
+//     const index = vaccineList.findIndex(f => f.id == id);
 
-    if (index > 0){
-        vaccineList.splice(index, 1)
-        res.status(200).json(vaccineList);
-    }else{
-        res.status(404).json({
-            des: "No delete data found."
-        });
-    }
-});
+//     if (index > 0){
+//         vaccineList.splice(index, 1)
+//         res.status(200).json(vaccineList);
+//     }else{
+//         res.status(404).json({
+//             des: "No delete data found."
+//         });
+//     }
+// });
 
 app.get('/getAppName', (req, res) => {
     const result = {
